244d1f342bb7f45aa9b453d12ce759b42af9fd91
DanielMcNeally <59981854+DanielMcNeally@users.noreply.github.com>
2020-01-23 16:57:44 -0600

    Windchill C update
    weather formulas

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 
-namespace BasicCalculator
+namespace Windchill_and_Dew_Point_Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** Windchill and Dew Point Calaculator ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Please enter the temperature");
 
             // int.Parse will take a string data type and convert it to an int data type
             int firstNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
+            Console.WriteLine("Please enter the relative humidity");
             int secondNumber = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            Console.WriteLine("Please enter the wind speed");
+            int thirdNumber = int.Parse(Console.ReadLine());
+                
+            decimal sum = firstNumber - ((100-secondNumber)/5);
+
+            //decimal sum = 35.74 + 0.6125*firstNumber-35.75*thirdNumber
 
             Console.WriteLine("The answer is " + sum);
         }
