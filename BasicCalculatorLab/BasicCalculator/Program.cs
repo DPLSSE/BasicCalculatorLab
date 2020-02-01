d9432e6fd8bb9336a4b134eae4bc6905f280ae26
Zakhutch18 <59981862+Zakhutch18@users.noreply.github.com>
2020-02-01 13:11:45 -0600

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -6,19 +6,34 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+            Console.WriteLine("*** BASIC WEATHER CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter the Temperature");
 
             // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            int Temperature = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
+            Console.WriteLine("Enter the Relative Humidity");
+            int Humidity = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("The answer is " + sum);
+            decimal Dewpoint = Temperature - 9 * (100 - Humidity) / 25;
+
+            Console.WriteLine("The Dew Point is " + Dewpoint);
+
+            Console.WriteLine("Enter The Wind Speed");
+
+            int WindSpeed = int.Parse(Console.ReadLine());
+
+            Double WindChill = 35.74 + .6215 * Temperature - 35.75 * (Math.Pow(WindSpeed, .16)) + .4275 * Temperature * (Math.Pow(WindSpeed, .16));
+
+
+            Console.WriteLine("The Wind Chill is " + WindChill);
         }
     }
 }
+
+
+
+
+
